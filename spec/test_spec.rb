57338f1618fb3979eef2cf259f81a1fabe4ddacd
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-06-22 13:28:10 +0530

    installs and configures rspec-rails gem

---
+++ spec/test_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe 'Test Rspec Configuration - Example Group' do
+    context 'Nested Example Group' do
+        it 'passes example' do
+            expect(4+5).to eq 9
+        end
+    end
+end
