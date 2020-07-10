6fab1d7fdcff3349b0563977f267babdc104baf1
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-07-10 16:46:08 +0530

    Sets Up Active Admin

--- Gemfile
+++ Gemfile
@@ -62,3 +62,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem 'devise', '~> 4.7', '>= 4.7.2'
+gem 'activeadmin', '~> 2.7'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
     validates :title, presence: true
-    # validates :description, presence: true
+    validates :description, presence: true
 end
