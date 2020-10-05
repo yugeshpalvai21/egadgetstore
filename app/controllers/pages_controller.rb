cea2a3c20970c40660679708f0d1e5d22a3a8817
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-10-05 16:49:12 +0530

    Deep Dive Controller and Views - Creates Helper Methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
     def home
+        @global_rating = 4
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def rating_in_stars rating
+        stars = ""
+        rating.times { stars << "*"}
+        stars
+    end
 end
