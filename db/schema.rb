927e8a8c98a73f958de3fbaae73d7c6d91397bcd
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-01-26 22:34:10 +0530

    removes column from products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_163719) do
+ActiveRecord::Schema.define(version: 2021_01_26_165723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
