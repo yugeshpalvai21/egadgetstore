6c2fd4a2daee5dc81a96d370c4e716ac10db2318
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-01-28 13:10:05 +0530

    Implements one-to-many association between Product and Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_170622) do
+ActiveRecord::Schema.define(version: 2021_01_28_070044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price", precision: 5, scale: 2
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times { |num| Category.create(name: "Category #{num}") }
+
+puts "5 Categories Created.."
+
+20.times { |num| Product.create(name: "Product #{num}", price: "#{rand(11..999)}.#{rand(10..99)}", category_id: rand(1..5)) }
+
+puts "20 Products Created..."
