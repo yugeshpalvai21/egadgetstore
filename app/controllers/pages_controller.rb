038b2c4bd9cf7a5e4d27e6fc597f187605b36c25
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-03-10 00:46:28 +0530

    generates random service tag using ajax request

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,8 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  def get_random_service_tag
+    @service_tag = "#{rand(10..99)}-#{[*('A'..'Z')].sample(8).join}-#{rand(2222..9999)}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/about', to: 'pages#about', as: 'about'
   get '/contact', to: 'pages#contact', as: 'contact'
+  get '/get_random_service_tag', to: 'pages#get_random_service_tag', as: 'get_random_service_tag'
 end
